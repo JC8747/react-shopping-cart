52dbad44537bb1846ac11e03f89cc1d05e091642
JC8747 <jonathan.chiaverini@gmail.com>
2020-03-05 21:21:34 -0500

    MVP functional, working stretch

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,17 +1,5 @@
-import React, { createContext, useState } from "react";
-import data from "../demoData";
+import { createContext } from "react";
 
-export const ProductContext = createContext();
+const ProductContext = createContext({});
 
-export const ProductProvider = props => {
-  const [shop, setShop] = useState({
-    cart: [],
-    price: 0,
-    items: [...data]
-  });
-  return (
-    <ProductContext.Provider value={[shop, setShop]}>
-      {props.children}
-    </ProductContext.Provider>
-  );
-};
+export default ProductContext;
